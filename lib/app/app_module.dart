dfbb4377b34b815bf740df8e7dee0f67667abbdf
Vinicius Trentin <vtrentin15@gmail.com>
2023-09-13 10:09:31 -0300

    [#1] home module implementada

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:intoxianime/app/modules/home/presenter/pages/home_page.dart';
+import 'package:intoxianime/app/modules/home/home_module.dart';
 
 class AppModule extends Module {
   @override
@@ -7,6 +7,6 @@ class AppModule extends Module {
 
   @override
   void routes(RouteManager r) {
-    r.child('/', child: (context) => const HomePage());
+    r.module('/', module: HomeModule());
   }
 }
