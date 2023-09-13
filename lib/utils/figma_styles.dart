352215b4a4123a9834fc656c9ab31d789fcc98c6
Vinicius Trentin <vtrentin15@gmail.com>
2023-09-13 09:15:09 -0300

    [#1] Pacotes adicionados ao projeto, configuração de cores e estilos de texto para o projeto.

---
+++ lib/utils/figma_styles.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FigmaColors {
+  const FigmaColors();
+
+  static const Color purplepurple50 = Color(0xfff3eefc);
+  static const Color purplepurple100 = Color(0xffd8cbf7);
+  static const Color purplepurple200 = Color(0xffc6b2f3);
+  static const Color purplepurple300 = Color(0xffab8eee);
+  static const Color purplepurple400 = Color(0xff9b79ea);
+  static const Color purplepurple500 = Color(0xff8257e5);
+  static const Color purplepurple600 = Color(0xff764fd0);
+  static const Color purplepurple700 = Color(0xff5c3ea3);
+  static const Color purplepurple800 = Color(0xff48307e);
+  static const Color purplepurple900 = Color(0xff372560);
+  static const Color orangeorange50 = Color(0xffffefeb);
+  static const Color orangeorange100 = Color(0xffffccc2);
+  static const Color orangeorange200 = Color(0xffffb4a4);
+  static const Color orangeorange300 = Color(0xffff927b);
+  static const Color orangeorange400 = Color(0xffff7d61);
+  static const Color orangeorange500 = Color(0xffff5c3a);
+  static const Color orangeorange600 = Color(0xffe85435);
+  static const Color orangeorange700 = Color(0xffb54129);
+  static const Color orangeorange800 = Color(0xff8c3320);
+  static const Color orangeorange900 = Color(0xff6b2718);
+  static const Color yelloworange50 = Color(0xfffff9ec & 0xffe6fbef);
+  static const Color yelloworange100 = Color(0xffffebc4 & 0xffb1f1ce);
+  static const Color yelloworange200 = Color(0xffffe2a7 & 0xff8cebb6);
+  static const Color yelloworange300 = Color(0xffffd47f & 0xff57e295);
+  static const Color yelloworange400 = Color(0xffffcc66 & 0xff36dc81);
+  static const Color yelloworange500 = Color(0xffffbf40 & 0xff04d361);
+  static const Color yelloworange600 = Color(0xffe8ae3a & 0xff04c058);
+  static const Color yelloworange700 = Color(0xffb5882d & 0xff039645);
+  static const Color yelloworange800 = Color(0xff8c6923 & 0xff027435);
+  static const Color yelloworange900 = Color(0xff6b501b & 0xff025929);
+  static const Color greengreen50 = Color(0xffe6fbef);
+  static const Color greengreen100 = Color(0xffb1f1ce);
+  static const Color greengreen200 = Color(0xff8cebb6);
+  static const Color greengreen300 = Color(0xff57e295);
+  static const Color greengreen400 = Color(0xff36dc81);
+  static const Color greengreen500 = Color(0xff04d361);
+  static const Color greengreen600 = Color(0xff04c058);
+  static const Color greengreen700 = Color(0xff039645);
+  static const Color greengreen800 = Color(0xff027435);
+  static const Color greengreen900 = Color(0xff025929);
+  static const Color graygray50 = Color(0xffeaeaea);
+  static const Color graygray100 = Color(0xffbebebf);
+  static const Color graygray200 = Color(0xff9e9ea0);
+  static const Color graygray300 = Color(0xff727275);
+  static const Color graygray400 = Color(0xff56565a);
+  static const Color graygray500 = Color(0xff2c2c31);
+  static const Color graygray600 = Color(0xff28282d);
+  static const Color graygray700 = Color(0xff1f1f23);
+  static const Color graygray800 = Color(0xff18181b);
+  static const Color graygray900 = Color(0xff121215);
+}
+
+class FigmaTextStyles {
+  const FigmaTextStyles();
+  static TextStyle title = GoogleFonts.roboto(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+    height: 1.6,
+  );
+  static TextStyle label = GoogleFonts.roboto(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    height: 1.6,
+  );
+  static TextStyle about = GoogleFonts.roboto(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    height: 1.6,
+  );
+  static TextStyle more = GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    height: 1.6,
+  );
+  static TextStyle label2 = GoogleFonts.roboto(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    height: 20.8 / 12,
+  );
+  static TextStyle about2 = GoogleFonts.roboto(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    height: 19.2 / 16,
+  );
+  static TextStyle publication = GoogleFonts.roboto(
+    fontSize: 10,
+    fontWeight: FontWeight.w300,
+  );
+  static TextStyle history = GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+     height: 21 / 14,
+  );
+}
